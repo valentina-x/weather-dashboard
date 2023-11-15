45387073604f44457e76f318ce3ca2b03f617261
Valentina <xzxz100.95@mail.ru>
2023-11-15 17:51:17 +0200

    добавление useRef / изменение структуры

--- src/Components/ToggleButtons/index.tsx
+++ src/Components/ToggleButtons/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ToggleButtonsStyles from './style.module.scss';
 
 interface IToggleButtonsProps {
