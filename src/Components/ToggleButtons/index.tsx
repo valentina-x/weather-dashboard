1559b96b11eff7d462868d55ddf3bf3ff2d20ac3
Valentina <xzxz100.95@mail.ru>
2023-11-15 17:50:01 +0200

    добавление useRef / изменение структуры

--- src/Components/ToggleButtons/index.tsx
+++ src/Components/ToggleButtons/index.tsx
@@ -1,43 +1,63 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ToggleButtonsStyles from './style.module.scss';
 
 interface IToggleButtonsProps {
   onChangeTypeWeather: (typeWeather: string) => void;
 }
 
+interface IButtonWeather {
+  className: string;
+  children: React.ReactNode;
+  onClick: () => void;
+  isActive: boolean;
+}
+
 const ToggleButtons: React.FC<IToggleButtonsProps> = ({ onChangeTypeWeather }) => {
-  const toggleClass = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    const target = e.target as HTMLElement;
-    const buttons: NodeListOf<Element> = document.querySelectorAll(
-      `.${ToggleButtonsStyles.buttons__button}`,
+  const [activeButton, setActiveButton] = useState<string>('forecast');
+
+  const ButtonWeather: React.FC<IButtonWeather> = ({ className, children, onClick, isActive }) => {
+    const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+    const handleButtonClick = () => {
+      if (buttonRef.current) {
+        const buttons = document.querySelectorAll(`.${ToggleButtonsStyles.buttons__button}`);
+        buttons.forEach((btn) => btn.classList.remove(`${ToggleButtonsStyles.buttons__button_active}`));
+        buttonRef.current.classList.add(`${ToggleButtonsStyles.buttons__button_active}`);
+      }
+      setActiveButton(activeButton === 'forecast' ? 'onecall': 'forecast');
+      if (onClick) {
+        onClick();
+      }
+    };
+
+    return (
+      <button
+        type='button'
+        className={`${className} ${isActive ? ToggleButtonsStyles.buttons__button_active : ''}`}
+        ref={buttonRef}
+        onClick={handleButtonClick}
+      >
+        {children}
+      </button>
     );
-    buttons.forEach((button) => {
-      button.classList.remove(`${ToggleButtonsStyles.buttons__button_active}`);
-    });
-    if (target.classList.contains(`${ToggleButtonsStyles.buttons__button}`)) {
-      target.classList.add(`${ToggleButtonsStyles.buttons__button_active}`);
-    }
   };
+
   return (
     <div className={`${ToggleButtonsStyles.buttons}`}>
-      <div
-        className={`${ToggleButtonsStyles.buttons__button} ${ToggleButtonsStyles.buttons__button_active}`}
-        onClick={(e) => {
-          toggleClass(e);
-          onChangeTypeWeather('forecast');
-        }}
+      <ButtonWeather
+        className={`${ToggleButtonsStyles.buttons__button} ${activeButton === 'forecast' ? ToggleButtonsStyles.buttons__button_active : ''}`}
+        onClick={() => onChangeTypeWeather('forecast')}
+        isActive={activeButton === 'forecast'}
       >
         На сегодня
-      </div>
-      <div
+      </ButtonWeather>
+      <ButtonWeather
         className={`${ToggleButtonsStyles.buttons__button}`}
-        onClick={(e) => {
-          toggleClass(e);
-          onChangeTypeWeather('onecall');
-        }}
+        onClick={() => onChangeTypeWeather('onecall')}
+        isActive={activeButton === 'onecall'}
       >
         На неделю
-      </div>
+      </ButtonWeather>
     </div>
   );
 };
